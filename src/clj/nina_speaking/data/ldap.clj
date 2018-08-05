01eaa19291afe97d078372a8c5c6c35d1d3661ab
Toby Tripp <toby.tripp+github@gmail.com>
2018-08-05 18:46:58 -0500

    Add search example

--- src/clj/nina_speaking/data/ldap.clj
+++ src/clj/nina_speaking/data/ldap.clj
@@ -1,6 +1,8 @@
 (ns nina-speaking.data.ldap
   (:require [clj-ldap.client :as ldap]))
 
+(def root-dn "dc=thetripps,dc=org")
+
 (def ldap-server
   (ldap/connect {:host "ldap"
                  :bind-dn "cn=admin,dc=thetripps,dc=org"
@@ -9,10 +11,7 @@
 
 (ldap/get ldap-server "cn=admin,dc=thetripps,dc=org")
 
-(ldap/add ldap-server "cn=people,ou=groups,dc=thetripps,dc=org"
-          {:objectClass #{"top" "posixGroup"}
-           :gidNumber 678})
+(ldap/search ldap-server (str "ou=people," root-dn))
 
-(ldap/add ldap-server "uid=test,ou=people,dc=thetripps,dc=org"
-          {:objectClass #{"top" "account"}
-           :uid test})
+
+
